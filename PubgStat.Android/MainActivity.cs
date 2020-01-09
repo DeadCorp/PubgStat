d8606126a75d5b6f855e8f5029fb459935834a27
cipidris <cipidris@gmail.com>
2020-01-09 18:30:40 +0200

    Some end

--- PubgStat.Android/MainActivity.cs
+++ PubgStat.Android/MainActivity.cs
@@ -21,6 +21,8 @@ namespace PubgStat.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            this.RequestedOrientation = ScreenOrientation.Portrait;
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
